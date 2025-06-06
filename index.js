0a5874e04bf7ab3de9879da5c45e8f8accb8a65d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-06 13:02:35 +0200

    fix: replace default zero by clearing input on firsst digit press using newLine flag

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let newLine = true;
+
 const digitBtnPressed = (button) => {
-  const currentValue = document.getElementById("inputBox").value;
-  document.getElementById("inputBox").value = currentValue + button;
+  if (newLine) {
+    document.getElementById("inputBox").value = button;
+    newLine = false;
+  } else {
+    const currentValue = document.getElementById("inputBox").value;
+    document.getElementById("inputBox").value = currentValue + button;
+  }
 };
